be835b4d5fb218362887232a60dd9a1e0512d8be
Facudlomas <Facundo.mosquera.cala@gmail.com>
2024-12-18 20:15:14 +0000

    tarde pero seguro

--- src/app.py
+++ src/app.py
@@ -1,4 +1,60 @@
-from utils import db_connect
-engine = db_connect()
+#resumen todo junto pra demas proyectos con sus respectivos cambios
+import pandas as pd
 
-# your code here
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+
+# modelado
+from sklearn.linear_model import LogisticRegression
+
+# escalado
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+
+# encoding
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OrdinalEncoder
+
+# metricas
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+
+# optimizar
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
+
+archivo= "https://raw.githubusercontent.com/4GeeksAcademy/logistic-regression-project-tutorial/main/bank-marketing-campaign-data.csv"
+
+df_original = pd.read_csv(archivo, sep=";")
+df = pd.read_csv(archivo, sep=";")
+
+
+datos,atributos=df.shape
+print(f"tenemos {datos} de datos y {atributos} atributos.")
+print()
+
+# Valores duplicados
+
+duplicados = df.duplicated()
+num_duplicados = duplicados.sum()
+
+print(f"tenemos {num_duplicados} datos duplicados")
+print()
+
+df_duplicados = df[duplicados]
+
+df_sin_duplicados = df.drop_duplicates()
+
+df=df_sin_duplicados
+
+# Obtener información sobre tipos de datos y valores no nulos
+df.info()
+#eliminar lo que no aporta valor a la prediccion
+df.drop(["contact", "month", "day_of_week","duration"], axis = 1, inplace = True)
+
+df.job.value_counts()
